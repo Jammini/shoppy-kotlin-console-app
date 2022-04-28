8305788b53cefbf89206d66450d58bd39e60ca6e
jammini <systemdata227@gmail.com>
2022-04-29 02:08:33 +0900

    사용자가 기대한 값을 입력하지 않을 때 입력을 재요청하는 StringExtension 함수 추가

--- src/main/kotlin/screen/ShoppingCategory.kt
+++ src/main/kotlin/screen/ShoppingCategory.kt
@@ -1,5 +1,7 @@
 package screen
 
+import extensions.getNotEmptyString
+
 class ShoppingCategory {
     fun showCategories() {
         val categories = arrayOf("패션", "전자기기", "반려동물용품")
@@ -8,14 +10,9 @@ class ShoppingCategory {
         }
         println("=> 장바구니로 이동하시려면 #을 입력해주세요")
 
-        var selectedCategory = readLine()
-
-        while (selectedCategory.isNullOrBlank()) {
-            println("값을 압력해주세요.")
-            selectedCategory = readLine()
-        }
+        val selectedCategory = readLine().getNotEmptyString()
         if (selectedCategory == "#") {
-            val shoppingCart= ShoppingCart()
+            val shoppingCart = ShoppingCart()
             shoppingCart.showCartItems()
         } else {
             if (categories.contains(selectedCategory)) {
